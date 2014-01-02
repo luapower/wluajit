cd8645d7809af8e675e3b47883713660cd224fb5
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-03 00:13:39 +0200

    init

---
+++ csrc/wluajit/build-mingw32.sh
@@ -0,0 +1,6 @@
+LUAJIT_SRC=../luajit/src/src
+BIN_DIR=../../bin/mingw32
+windres luajit.rc luajit.o
+gcc wmain.c luajit.o $LUAJIT_SRC/luajit.c -O3 -s -o $BIN_DIR/wluajit.exe -mwindows -llua51 -I$LUAJIT_SRC -L$BIN_DIR
+rm luajit.o
+upx $BIN_DIR/wluajit.exe

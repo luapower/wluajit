aee059d39065a6db0c84eaa2d47d7dc1fc886b1e
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-14 14:41:23 +0200

    added $CFLAGS option and recompiled with -O2

--- csrc/wluajit/build-mingw32.sh
+++ csrc/wluajit/build-mingw32.sh
@@ -1,5 +1,5 @@
 LUAJIT_SRC=../luajit/src/src
 BIN_DIR=../../bin/mingw32
 windres luajit.rc luajit.o
-gcc wmain.c luajit.o $LUAJIT_SRC/luajit.c -O3 -s -o $BIN_DIR/wluajit.exe -mwindows -llua51 -I$LUAJIT_SRC -L$BIN_DIR
+gcc $CFLAGS wmain.c luajit.o $LUAJIT_SRC/luajit.c -o $BIN_DIR/wluajit.exe -mwindows -llua51 -I$LUAJIT_SRC -L$BIN_DIR
 rm luajit.o

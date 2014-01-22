9435e67d3735deda38624aa676570d1f02b5b060
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-22 06:21:06 +0200

    unimportant

--- wluajit.cmd
+++ wluajit.cmd
@@ -1,5 +1,4 @@
-@echo off
-rem execute wluajit from this directory so that media files are found using relative paths.
-pushd %~dp0
-start bin\mingw32\wluajit %*
-popd
+@rem wluajit wrapper that starts wluajit in this directory so that scripts can access their files using relative paths.
+@pushd %~dp0
+@start bin\mingw32\wluajit %*
+@popd
